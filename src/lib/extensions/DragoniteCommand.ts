a2d1b06e0717766e81d52c8ba9e3208c8add6e81
Jeroen Claassens <support@favware.tech>
2022-01-20 00:51:50 +0100

    refactor: fix resolving of client permissions

--- src/lib/extensions/DragoniteCommand.ts
+++ src/lib/extensions/DragoniteCommand.ts
@@ -1,10 +1,13 @@
 import { Command } from '@sapphire/framework';
 import { PermissionFlagsBits } from 'discord-api-types/v9';
+import { Permissions } from 'discord.js';
 
 export abstract class DragoniteCommand extends Command {
   public constructor(context: Command.Context, options: Command.Options) {
+    const resolvedPermissions = new Permissions(options.requiredClientPermissions).add(PermissionFlagsBits.EmbedLinks);
+
     super(context, {
-      requiredClientPermissions: [options.requiredClientPermissions ?? [], PermissionFlagsBits.EmbedLinks],
+      requiredClientPermissions: resolvedPermissions,
       ...options
     });
   }
