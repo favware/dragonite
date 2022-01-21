fa9d51c21d7192c8cd0a09d754e19ece90d2d154
Jeroen Claassens <support@favware.tech>
2022-01-21 23:27:09 +0100

    feat: add sweeper options

--- src/config.ts
+++ src/config.ts
@@ -3,9 +3,10 @@ process.env.NODE_ENV ??= 'development';
 
 import { envParseBoolean, envParseInteger, envParseString } from '#lib/env';
 import { srcFolder } from '#utils/constants';
+import { minutes } from '#utils/functions/time';
 import { LogLevel } from '@sapphire/framework';
 import { ScheduledTaskRedisStrategy } from '@sapphire/plugin-scheduled-tasks/register-redis';
-import type { ActivitiesOptions, ClientOptions, ExcludeEnum, WebhookClientData } from 'discord.js';
+import { ActivitiesOptions, ClientOptions, ExcludeEnum, Options, WebhookClientData } from 'discord.js';
 import type { ActivityTypes } from 'discord.js/typings/enums';
 import { config } from 'dotenv-cra';
 import { join } from 'path';
@@ -56,6 +57,13 @@ export const CLIENT_OPTIONS: ClientOptions = {
   allowedMentions: { users: [], roles: [] },
   presence: { activities: parsePresenceActivity() },
   logger: { level: envParseString('NODE_ENV') === 'production' ? LogLevel.Info : LogLevel.Debug },
+  sweepers: {
+    ...Options.defaultSweeperSettings,
+    messages: {
+      interval: minutes.toSeconds(3),
+      lifetime: minutes.toSeconds(15)
+    }
+  },
   tasks: {
     strategy: new ScheduledTaskRedisStrategy({
       bull: {
