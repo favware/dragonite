84c892b17f1ae188e5169e610a78bddda5c555ee
r-priyam <roy_priyam@outlook.com>
2022-09-25 10:26:19 +0530

    chore: remove redundant command behavior

--- src/lib/setup.ts
+++ src/lib/setup.ts
@@ -3,13 +3,8 @@ import '#root/config';
 import '#utils/Sanitizer/initClean';
 import '@sapphire/plugin-logger/register';
 
-// Import everything else:
-import { ApplicationCommandRegistries, RegisterBehavior } from '@sapphire/framework';
-
 import * as colorette from 'colorette';
 import { inspect } from 'util';
 
 inspect.defaultOptions.depth = 1;
 colorette.createColors({ useColor: true });
-
-ApplicationCommandRegistries.setDefaultBehaviorWhenNotIdentical(RegisterBehavior.Overwrite);
