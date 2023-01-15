4df1b0c3e404ede89fd3faab82038f9c8b17886e
Jeroen Claassens <support@favware.tech>
2023-01-15 04:25:14 +0100

    chore: change to `rimraf` for cleaning

--- scripts/clean.mjs
+++
@@ -1,6 +0,0 @@
-import { rm } from 'node:fs/promises';
-
-const rootFolder = new URL('../', import.meta.url);
-const distFolder = new URL('dist/', rootFolder);
-
-await rm(distFolder, { recursive: true, force: true });
